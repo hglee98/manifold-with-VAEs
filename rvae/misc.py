[fix] Calculate KL divergence in sigma optimization phase

rvae/misc.py
@@ -10,7 +10,7 @@ import math
 class DistSqKL(torch.autograd.Function):
     @staticmethod
     def forward(ctx, net, p0, p1):
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        device = "cuda:3" if torch.cuda.is_available() else "cpu"
         b_sz = p0.shape[0]
         with torch.no_grad():
             with torch.enable_grad():
@@ -59,7 +59,7 @@ def connecting_geodesic(net, p0, p1, optim=torch.optim.SGD, max_iter=25, n_nodes
     """Computes the logmap of the geodesic with endpoints 
     p0, p1 \in M by minimizing the curve energy.
     """
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda:3" if torch.cuda.is_available() else "cpu"
     # The line below is written assuming p1 is the mean
     curve = CubicSpline(p0, p1, num_nodes=n_nodes, device=device)
     
@@ -86,7 +86,7 @@ def connecting_geodesic(net, p0, p1, optim=torch.optim.SGD, max_iter=25, n_nodes
     return curve, curve_energies.detach_()
 
 
-def log_bm_krn(x, y, t, model):
+def log_bm_krn(x, y, t, model, device):
     """Log pdf of a Brownian motion (BM) transition kernel.
     
     params:
@@ -103,8 +103,8 @@ def log_bm_krn(x, y, t, model):
     _, logdet_y = model.metric(y).slogdet()
     log_H = (logdet_x - logdet_y)/2
     l_sq = DistSqKL.apply(model, x, y)
-
-    return -d/2 * torch.log(2 * pi * t) + log_H - l_sq/(2 * t)
+    result = -d/2 * torch.log(2 * pi * t) + log_H - l_sq/(2 * t)
+    return result
 
 
 def brownian_motion_sample(num_steps, num_samples, dim, t, init_point, model):

rvae/models/vae.py
@@ -13,7 +13,7 @@ class RVAE(nn.Module):
     """Variational Autoencoder with a Riemannian Brownian motion prior."""
     
     def __init__(self, in_dim, latent_dim, num_centers, enc_layers, 
-                 dec_layers, act, out_fn, rbf_beta, rec_b):
+                 dec_layers, act, out_fn, rbf_beta, rec_b, device):
         """Constructor specs.
         
         Params:
@@ -56,6 +56,7 @@ class RVAE(nn.Module):
         self._latent_codes = None
         self.pr_means = torch.nn.Parameter(torch.zeros(latent_dim), requires_grad=True)
         self.pr_t = torch.nn.Parameter(torch.ones(1), requires_grad=True)
+        self.device = device
 
     def encode(self, x):
         h = self.encoder(x)
@@ -70,7 +71,7 @@ class RVAE(nn.Module):
         return z, q_mu, q_t
 
     def _update_latent_codes(self, data_loader):
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        device = self.device
         codes = []
         for _, (data, labels) in enumerate(data_loader):
             if data.dim() == 4:
@@ -84,7 +85,7 @@ class RVAE(nn.Module):
         self._latent_codes = torch.cat(codes, dim=0).view(-1, self.latent_dim)
     
     def _update_RBF_centers(self, beta=None):
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        device = self.device
         kmeans = KMeans(n_clusters = self.num_centers)  # self.num_centers
         data = self._latent_codes.detach().cpu().numpy()
         kmeans.fit(data)
@@ -96,7 +97,7 @@ class RVAE(nn.Module):
             idx = np.random.randint(self.num_centers)
             self.pr_means = torch.nn.Parameter(self.p_sigma._modules['0'].points.data[idx])
         else:
-            device = "cuda:0" if torch.cuda.is_available() else "cpu"
+            device = self.device
             kmeans = KMeans(n_clusters=1)
             kmeans.fit(self._latent_codes.detach().cpu().numpy())
             self.pr_means.data = torch.from_numpy(kmeans.cluster_centers_.astype(np.float32)).to(device)
@@ -182,7 +183,7 @@ class VAE(nn.Module):
     """Variational autoencoder with an RBF network for the decoder."""
 
     def __init__(self, in_dim, latent_dim, num_centers, num_components, enc_layers, 
-                 dec_layers, act, out_fn, rbf_beta, rec_b):
+                 dec_layers, act, out_fn, rbf_beta, rec_b, device):
         """Constructor specs.
         
         Params:
@@ -227,13 +228,14 @@ class VAE(nn.Module):
             nnj.Sqrt()
         )
         self._latent_codes = None
+        self.device = device
 
         # if num_components > 1 assume VampPrior
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
         if num_components > 1:
             self.means = NonLinear(num_components, in_dim, activation=nn.Hardtanh(min_val=0., max_val=1.))
             # self.means.linear.weight.normal_(mean=0., std=0.01)
-            self.dummy_input = torch.tensor(torch.eye(num_components, num_components), requires_grad=False, device=device)
+            self.dummy_input = torch.tensor(torch.eye(num_components, num_components),
+                                            requires_grad=False, device=self.device)
 
     def encode_prior(self):
         u = self.means(self.dummy_input)
@@ -248,7 +250,7 @@ class VAE(nn.Module):
         return mu + eps * var.sqrt()
 
     def _update_latent_codes(self, data_loader):
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        device = self.device
         codes = []
         for _, (data, labels) in enumerate(data_loader):
             data = data.to(device)
@@ -258,7 +260,7 @@ class VAE(nn.Module):
         self._latent_codes = torch.cat(codes, dim=0).view(-1, self.latent_dim)
 
     def _update_RBF_centers(self, beta=None):
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        device = self.device
         kmeans = KMeans(n_clusters=self.num_centers)
         kmeans.fit(self._latent_codes.detach().cpu().numpy())
         self.p_sigma._modules['0'].points.data = torch.from_numpy(kmeans.cluster_centers_.astype(np.float32)).to(device)

rvae/variational_inference/train.py
@@ -23,7 +23,7 @@ def train_rvae(epoch, train_loader, batch_size, model, optimizer, log_invl, devi
         if model.switch:
             p_sigma = torch.ones(1).to(device)
         
-        loss, log_pxz, kld = elbo_rvae(data, p_mu, p_sigma, z, q_mu, q_t.squeeze(), model, beta)
+        loss, log_pxz, kld = elbo_rvae(data, p_mu, p_sigma, z, q_mu, q_t.squeeze(), model, beta, device)
         loss.backward()
         train_loss += loss.item()
         train_kld += kld
@@ -54,7 +54,7 @@ def test_rvae(test_loader, batch_size, model, device):
             # data = data.view(-1, data.shape[-1] * data.shape[-2]).to(device)
             data = data.to(device)
             p_mu, p_sigma, z, q_mu, q_t = model(data)
-            loss = elbo_rvae(data, p_mu, p_sigma, z, q_mu, q_t, model, 1.)
+            loss = elbo_rvae(data, p_mu, p_sigma, z, q_mu, q_t, model, 1, device)
             test_loss += loss[0]    # ELBO
             test_rec += loss[1]     # log conditional
             test_kld += loss[2]     # KL div

rvae/vizualization/visualization.py
@@ -110,7 +110,7 @@ def plot_latent_space(model, pretrained_path, inp_dim, target_dim, data_loader, 
         if isinstance(model, RVAE):
             _, _, z, _, _ = model(x.view(-1, inp_dim).to(device))
         else:
-            _, _, _, z, _, _, _ = model(x.view(-1, inp_dim).to(device))
+            _, _, z, _, _, _, _ = model(x.view(-1, inp_dim).to(device))
         samples.append(z)
         labels.append(y)
 
@@ -127,7 +127,9 @@ def plot_latent_space(model, pretrained_path, inp_dim, target_dim, data_loader, 
         ax = fig.add_subplot(111, projection='3d')
         samples = torch.stack(samples, dim=0).view(-1, 3).detach().cpu().numpy()
         labels = torch.stack(labels).view(-1).detach().cpu().numpy()
+        ax.set_title(session)
         ax.scatter(samples[:, 0], samples[:, 1], samples[:, 2], s=0.5, c=labels)
+
 
     if isinstance(model, RVAE):
         model_name = 'RVAE'